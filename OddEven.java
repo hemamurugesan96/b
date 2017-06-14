f5c2595bc1046d4aa6fd3e1d51cc87f388e0db71
hemamurugesan96 <hemamurugesanece@gmail.com>
2017-06-14 14:47:40 +0530

    OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+class OddEven
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("Even Number");
+}
+else
+{
+System.out.println("odd Number");
+}
+}
+}
